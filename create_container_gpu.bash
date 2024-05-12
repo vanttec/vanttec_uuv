c169cba5a287f3624f9009d8617a4fb75f89c15a
ivan <you@example.com>
2024-05-11 20:06:50 -0600

    Changes

--- create_container_gpu.bash
+++ create_container_gpu.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Settings used for accessing X11 server and thus getting graphics within the container
-DOCKER_GRAPHICS_ARGS="--env DISPLAY --env QT_X11_NO_MITSHM=1 --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --device=/dev/dri:/dev/dri"
+DOCKER_GRAPHICS_ARGS="--env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --device=/dev/dri:/dev/dri"
 
 xhost +
 
@@ -9,7 +9,7 @@ docker run -it -d\
     $DOCKER_GRAPHICS_ARGS \
     --name uuv \
     --gpus all \
-    --privileged \
     -v "$PWD:/ws/vanttec_uuv/src" \
+    --network host \
     uuv \
     /bin/bash
