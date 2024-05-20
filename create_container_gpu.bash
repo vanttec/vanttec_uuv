cbf374876df501a3e2fa27a59d466e1c165190de
ivan <you@example.com>
2024-05-20 13:02:32 -0600

    Once again working the GUI... F

--- create_container_gpu.bash
+++ create_container_gpu.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Settings used for accessing X11 server and thus getting graphics within the container
-DOCKER_GRAPHICS_ARGS="--env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --device=/dev/dri:/dev/dri"
+DOCKER_GRAPHICS_ARGS="--env DISPLAY --env QT_X11_NO_MITSHM=1 --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --device=/dev/dri:/dev/dri"
 
 xhost +
 
@@ -10,6 +10,5 @@ docker run -it -d\
     --name uuv \
     --gpus all \
     -v "$PWD:/ws/vanttec_uuv/src" \
-    --network host \
     uuv \
     /bin/bash
