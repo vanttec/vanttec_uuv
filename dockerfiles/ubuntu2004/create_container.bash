59248904bc98d6bd2d779762c2550eee5c65e5e9
BlueE-05 <danaetorres35@gmail.com>
2024-06-18 14:09:58 -0600

    Working version

--- dockerfiles/ubuntu2004/create_container.bash
+++ dockerfiles/ubuntu2004/create_container.bash
@@ -9,7 +9,7 @@ docker run -it -d\
     $DOCKER_GRAPHICS_ARGS \
     --name uuv \
     --privileged \
-    -v "$PWD:/ws/vanttec_uuv/src" \
+    -v "${PWD%/* / *}:/ws/vanttec_uuv/src" \
     -v "/dev/bus/usb/:/dev/bus/usb" \
     uuv \
     /bin/bash
