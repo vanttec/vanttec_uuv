0f463ee22bb3e2af1bac4987a22e97a15cd4742f
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2025-05-30 21:01:40 -0600

    working bash para imagenes y contenedor

--- dockerfiles/ros_nuevo/build_and_run.sh
+++ dockerfiles/ros_nuevo/build_and_run.sh
@@ -1,34 +1,43 @@
-# RUN with ./build_and_run.sh
+# /bin/bash
+#! RUN with ./build_and_run.sh
 
-#!/bin/bash
-
-# names
+# Constants
 IMAGE_NAME="ros_boost_image"
 CONTAINER_NAME="ros_boost_container"
-DOCKERFILE_DIR="dockerfiles/ros_nuevo/dockerfile"   
-WORKDIR="/root"      # path inside container
+DOCKERFILE_DIR="$(pwd)"   
+WORKDIR="/root"                     
+HOST_SRC_PATH="$(cd ../.. && pwd)/src"
 
-# image config
+
+# Before running, check paths plsplspls
+#echo "[DEBUG] Current directory: $(pwd)"
+#echo "[DEBUG] DOCKERFILE_DIR: $DOCKERFILE_DIR"
+#echo "[DEBUG] HOST_SRC_PATH: $HOST_SRC_PATH"
+
+# Image
 echo "[+] Building Docker image: $IMAGE_NAME"
-docker build -t $IMAGE_NAME $DOCKERFILE_DIR
+docker build -t $IMAGE_NAME "$DOCKERFILE_DIR"
 
-# check image
+# Build check
 if [ $? -ne 0 ]; then
     echo "[-] Docker build failed. Exiting."
     exit 1
 fi
 
-# Run ZED 
-echo "[+] Starting ZED Explorer on host..."
-/usr/local/zed/tools/ZED_Explorer &
+# TODO: check that this starts ZED
+#echo "[+] Starting ZED Explorer on host..."
+#/usr/local/zed/tools/ZED_Explorer &
 
-# container config
+# Container
 echo "[+] Running Docker container: $CONTAINER_NAME"
-docker run -it --name $CONTAINER_NAME --rm $IMAGE_NAME bash
+docker run -it \
+    --name $CONTAINER_NAME \
+    --rm \
+    --privileged \
+    -v /dev:/dev \
+    -v "$HOST_SRC_PATH:/ws/src" \
+    $IMAGE_NAME bash
 
-# After container exits, kill ZED
-echo "[*] Cleaning up ZED Explorer"
-pkill -f ZED_Explorer
-
-
-
+# TODO: check that this ends ZED process
+#echo "[*] Cleaning up ZED Explorer"
+#pkill -f ZED_Explorer
